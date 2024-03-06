d208366e346f0abf10c8985cf037dc15d7c30fef
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-03-06 10:37:36 -0500

    fix: keep one peak per gene when joined to rnaseq data

--- modules/local/join_atac_rna/templates/join_atac_rna.R
+++ modules/local/join_atac_rna/templates/join_atac_rna.R
@@ -4,14 +4,20 @@ library(glue)
 library(readr)
 library(stringr)
 library(tidyr)
-main <- function(promoters_logfc = "${promoters_logfc}", rna_logfc = "${rna_logfc}",
-                 outfile = "${outfile}", logfile = "${logfile}", cluster_id = "${meta.id}") {
+main <- function(promoters_logfc = "${promoters_logfc}",
+                 rna_logfc = "${rna_logfc}",
+                 outfile = "${outfile}",
+                 logfile = "${logfile}",
+                 cluster_id = "${meta.id}") {
   dat_rna <- read_tsv(rna_logfc) %>% rename(gene_name = `...1`)
   dat_promoters <- read_tsv(promoters_logfc)
   genes_atac_rna <- dat_promoters %>%
     inner_join(dat_rna, by = "gene_name") %>%
     rename_with(~ str_replace(., ".x", "_atac"), ends_with(".x")) %>%
-    rename_with(~ str_replace(., ".y", "_rna"), ends_with(".y"))
+    rename_with(~ str_replace(., ".y", "_rna"), ends_with(".y")) %>%
+    group_by(gene_name) %>%
+    slice_max(n_bases_overlap) %>% # pick one peak per gene based on number of bases that overlap
+    ungroup()
   genes_atac_rna %>%
     mutate(cluster_id = cluster_id) %>%
     write_tsv(outfile)
@@ -24,6 +30,6 @@ main <- function(promoters_logfc = "${promoters_logfc}", rna_logfc = "${rna_logf
   write_lines(log_msg, logfile)
 }
 main()
-# main(promoters_logfc = "output/join_peaks_promoters/intersect.rank3.pdx.cluster1.alone.logFC_1.pval_0.05.hg19.promoters.tsv",
-#      rna_logfc = "data/cluster1_vs_other_DGE.csv",
+# main(promoters_logfc = "output/join_peaks_promoters/intersect.rank3.pdx.cluster1.DEseq2.hg19.promoters.atac-promoters.tsv",
+#      rna_logfc = "data/rank3.pdx.cluster1_RNA.DEseq2.tsv",
 #     outfile = 'tmp.tsv', logfile = 'tmp.log')
