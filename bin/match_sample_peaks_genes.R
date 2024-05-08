fa98799b0f7508aa14dbd248c1afc03f1aab888c
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-05-08 11:35:46 -0400

    feat: map new sample IDs from Rajesh

--- bin/match_sample_peaks_genes.R
+++ bin/match_sample_peaks_genes.R
@@ -8,11 +8,25 @@ library(tidyr)
 
 main <-
   function(metadata_infile = "assets/metadata_for_kelley.xlsx",
+           pdx_meta_infile = "assets/pdx_rank3_metadata.tsv",
            rna_counts_infile = "data/rna_counts_normalized.csv",
            atac_counts_infile = "data/raw_tmm_fpkm_batch_corrected_PDX_only.csv",
            peak_gene_infile = "output/reformat_bed_intersect/intersect.raw_tmm_fpkm_batch_corrected_PDX_only.gencode.v19.annotation.TSS_padded.reformat.bed") {
     # integrate RNA-seq with ATAC-seq
-    metadat <- readxl::read_excel(metadata_infile)
+    pdx_metadat <- read_tsv("assets/pdx_rank3_metadata.tsv") %>%
+      rename(sample_id_verbose = sample_id) %>%
+      mutate(
+        sample_id = str_extract(sample_id_verbose,
+          pattern = "Sample_\\d{1,2}_([a-zA-Z0-9]{6})_.*",
+          group = 1
+        )
+      ) %>%
+      select(sample_id, sample_id_verbose, ATAC_Id, rank3) %>%
+      rename_with(~ tolower(glue("pdx_{.x}")))
+    metadat <- readxl::read_excel(metadata_infile) %>%
+      mutate(sample_id = as.character(Sample_ID)) %>%
+      full_join(pdx_metadat, by = c("sample_id" = "pdx_sample_id"))
+
     rna_counts_norm <- read_csv(rna_counts_infile)
     atac_counts_norm <-
       read_csv(atac_counts_infile)
@@ -30,12 +44,10 @@ main <-
         names_to = "atac_id_sample",
         values_to = "atac_count"
       ) %>%
-      mutate(
-        atac_sample_id = str_extract(atac_id_sample,
-          pattern = "([a-zA-Z0-9]+)_S.*",
-          group = 1
-        )
-      ) %>%
+      mutate(atac_sample_id = str_extract(atac_id_sample,
+        pattern = "([a-zA-Z0-9]+)_S.*",
+        group = 1
+      )) %>%
       right_join(peak_gene_tss, relationship = "many-to-many")
 
     atac_ids <- atac_counts_norm_long %>%
@@ -49,7 +61,10 @@ main <-
         names_to = "rna_id_sample",
         values_to = "rna_count"
       ) %>%
-      mutate(rna_sample_id = str_remove(rna_id_sample, "^Sample_\\d+_"))
+      mutate(rna_sample_id = str_extract(rna_id_sample,
+        pattern = "Sample_\\d{1,2}_([a-zA-Z0-9]{6})_.*",
+        group = 1
+      ))
 
     rna_ids <- rna_counts_norm_long %>%
       select(rna_id_sample, rna_sample_id) %>%
@@ -60,41 +75,42 @@ main <-
         rna_id_meta = `Matching Bulk RNA ID`,
         atac_id_meta = ATAC_ID
       ) %>%
-      mutate(rna_id_meta = case_when(
-        rna_id_meta == "N" & !is.na(`Parth Found_RNA_ID`) ~ `Parth Found_RNA_ID`,
-        rna_id_meta == "N" ~ NA_character_,
-        TRUE ~ rna_id_meta
-      )) %>%
-      filter(!is.na(rna_id_meta)) %>%
-      select(atac_id_meta, rna_id_meta) %>%
+      mutate(
+        rna_id_meta = case_when(
+          rna_id_meta == "N" &
+            !is.na(`Parth Found_RNA_ID`) ~ `Parth Found_RNA_ID`,
+          rna_id_meta == "N" ~ NA_character_,
+          TRUE ~ rna_id_meta
+        )
+      ) %>%
+      # filter(!is.na(rna_id_meta)) %>%
+      select(pdx_sample_id_verbose, sample_id, atac_id_meta, rna_id_meta, pdx_rank3) %>%
       separate_longer_delim(rna_id_meta, "/") %>%
-      mutate(rna_id = str_remove(str_remove(rna_id_meta, "^Sample_\\d+_"), "^\\d{1,2}_")) %>%
       mutate(atac_id = str_extract(atac_id_meta,
         pattern = "([a-zA-Z0-9]+)",
         group = 1
       )) %>%
-      full_join(
-        atac_ids,
+      full_join(atac_ids,
         by = c("atac_id" = "atac_sample_id")
       ) %>%
-      full_join(
-        rna_ids,
-        by = c("rna_id" = "rna_sample_id")
+      full_join(rna_ids,
+        by = c("pdx_sample_id_verbose" = "rna_id_sample")
+      ) %>%
+      select(-rna_sample_id) %>%
+      rename(
+        rna_sample_id = pdx_sample_id_verbose,
+        rna_id = sample_id,
+        atac_sample_id = atac_id_sample
       )
     samples_mapped <- metadat_join %>%
-      filter(!is.na(atac_id), !is.na(rna_id)) %>%
-      rename(
-        rna_sample_id = rna_id,
-        atac_sample_id = atac_id
-      )
-
-    rna_counts_norm_long %>%
-      left_join(samples_mapped, relationship = "many-to-many")
+      filter(!is.na(atac_id), !is.na(rna_id))
 
     counts_join <- full_join(
       rna_counts_norm_long %>%
+        rename(rna_id = rna_sample_id) %>%
         right_join(samples_mapped, relationship = "many-to-many"),
       atac_counts_norm_long %>%
+        rename(atac_id = atac_sample_id) %>%
         right_join(samples_mapped, relationship = "many-to-many"),
       relationship = "many-to-many"
     )
