9f40c2ef483c9cde6f6870284042ae6a7d808eb9
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-05-28 10:06:28 -0400

    chore: bump walltime to 4 days

--- bin/slurm.sh
+++ bin/slurm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=20g
-#SBATCH --time=1-00:00:00
+#SBATCH --time=4-00:00:00
 #SBATCH --parsable
 #SBATCH -J "ccbr-1326"
 #SBATCH --mail-type=BEGIN,END,FAIL
