b315c1e224c40e90bebcf7198a40d70c44ce369e
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-06-26 15:43:53 -0400

    fix: write out column names

--- bin/filter_corr_tsv.R
+++ bin/filter_corr_tsv.R
@@ -9,7 +9,7 @@ main <- function(input_tsv = "output_2/correlations/gene_peak_corr.tsv",
   read_tsv(input_tsv) %>%
     filter(estimate > 0, p.value < pvalue_thresh) %>%
     arrange(desc(estimate)) %>%
-    write_tsv(output_tsv, col_names = FALSE)
+    write_tsv(output_tsv)
 }
 
 args <- commandArgs(trailingOnly = TRUE)
