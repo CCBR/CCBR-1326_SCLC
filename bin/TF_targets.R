2d0926dee78abdf5cf85e8278e665deebe6bb8e2
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-12-26 11:12:23 -0500

    fix: filter joined data

--- bin/TF_targets.R
+++ bin/TF_targets.R
@@ -18,7 +18,7 @@ atac_dat <- "data/rank3.pdx.cluster1.DEseq2.tsv data/rank3.pdx.cluster2.DEseq2.t
       rename(peak_coord = `...1`)
   }) %>%
   bind_rows() %>%
-  select(peak_coord, log2FoldChange, pvalue, padj, cluster) %>%
+  select(peak_coord, log2FoldChange, padj, cluster) %>%
   rename(
     log2fc_atac = log2FoldChange,
     padj_atac = padj
@@ -35,7 +35,7 @@ rna_dat <- "data/rank3.pdx.cluster1_RNA.DEseq2.edit.tsv data/rank3.pdx.cluster3_
       )
   }) %>%
   bind_rows() %>%
-  select(gene_name, log2FoldChange, pvalue, padj, cluster) %>%
+  select(gene_name, log2FoldChange, padj, cluster) %>%
   rename(
     log2fc_rna = log2FoldChange,
     padj_rna = padj,
@@ -48,11 +48,10 @@ dat <- corr_dat %>%
   left_join(atac_dat) %>%
   left_join(rna_dat) %>%
   select(TSS_gene_name, peak_coord, TF_list, cluster, atac_sample_id, rna_sample_id, atac_count, rna_count, log2fc_atac, log2fc_rna, padj_atac, padj_rna)
+
 dat_filt <- dat %>%
   filter(
-    !is.na(log2fc_atac), !is.na(log2fc_rna),
-    padj_atac < alpha_level, abs(log2fc_atac) >= log2fc_thresh,
-    padj_rna < alpha_level, abs(log2fc_rna) >= log2fc_thresh
+    !is.na(log2fc_atac), !is.na(log2fc_rna)
   )
 
 dat_filt %>%
@@ -60,18 +59,15 @@ dat_filt %>%
   separate_longer_delim(cols = TF_list, delim = ",") %>%
   rename(TF_name = TF_list)
 
-
 dat_filt %>%
   select(TSS_gene_name, peak_coord, cluster) %>%
-  unique()
-dat %>%
+  distinct()
+
+dat_filt %>%
   pull(TSS_gene_name) %>%
   unique() %>%
   length()
-rna_dat %>%
-  pull(TSS_gene_name) %>%
-  unique() %>%
-  length()
+
 corr_dat %>%
   left_join(motif_dat) %>%
   left_join(atac_dat) %>%
